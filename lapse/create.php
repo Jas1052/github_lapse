f815768e8bf940fa2ae473a711b8e38a5a5a238f
Liu <2017jliu@tjhsst.edu>
2017-01-12 14:18:30 -0500

    Added favicon and minor arrow changes

--- lapse/create.php
+++ lapse/create.php
@@ -57,41 +57,44 @@ $numOfCommits = count($commits);
 
 foreach ($commits as $commit) {
     $htmlFile = fopen("commits\\frame" . $counter . ".html", "w") or exit("Unable to write file: commits\\frame-" . $counter . ".html");
-    $content = "<!DOCTYPE html><html><body style='width:100%;height:100%;background-color:white;'>" .
+    $content = "<!DOCTYPE html><html><head><link rel='shortcut icon' href='../../www/img/favicon.ico' type='image/x-icon'/></head><body style='width:100%;height:100%;background-color:white;'>" .
                 "<div style='font-family:Segoe UI;color:blue;font-size:50px;position:absolute;top:0;left:15px;'>" .
-                $counter . "</div><div style='display:flex;align-items:center;justify-content:center;'><pre>";
+                $counter . "</div><div style='align-items:center;justify-content:center;'><pre>";
 
     try {
         $content .= $extractor->getFileAtCommit($filePath, $commit);
     } catch (Exception $e) { }
 
-    $content .= "</pre></div></body></html>\n" . 
-                "<script>
-                document.onkeydown = checkKey;
-                function checkKey(e) {
-                    e = e || window.event;
-                  if (e.keyCode == '37') {
-                       var fileName = location.href.split('/').slice(-1); 
-                       fileName = fileName.toString();
-                       var dotIndex = fileName.indexOf('.');
-                       fileCount = fileName.substring(5, dotIndex);
-                       var count = parseInt(fileCount);
-                       count--;
-                       document.location.href = 'frame' + count + '.html';
-                    }
-                    else if (e.keyCode == '39') {
-                       // right arrow
-                       var fileName = location.href.split('/').slice(-1); 
-                       fileName = fileName.toString();
-                       var dotIndex = fileName.indexOf('.');
-                       fileCount = fileName.substring(5, dotIndex);
-                       var count = parseInt(fileCount);
-                       count++;
-                       document.location.href = 'frame' + count + '.html';
-                  }
+    $content .= 
+    "</pre></div></body></html>\n" . 
+    "<script>
+    document.onkeydown = checkKey;
+    function checkKey(e) {
+        e = e || window.event;
+      if (e.keyCode == '37') {
+          var fileName = location.href.split('/').slice(-1); 
+          fileName = fileName.toString();
+          var dotIndex = fileName.indexOf('.');
+          fileCount = fileName.substring(5, dotIndex);
+          var count = parseInt(fileCount);
+          count--;
+          if(count>0){
+            document.location.href = 'frame' + count + '.html';
+         }
+        }
+        else if (e.keyCode == '39') {
+           // right arrow
+           var fileName = location.href.split('/').slice(-1); 
+           fileName = fileName.toString();
+           var dotIndex = fileName.indexOf('.');
+           fileCount = fileName.substring(5, dotIndex);
+           var count = parseInt(fileCount);
+           count++;
+           document.location.href = 'frame' + count + '.html';
+      }
 
-                }
-                </script>";
+    }
+    </script>";
     fwrite($htmlFile, $content);
 
     $htmlPath = "file:///" .  str_replace('\\', '/', dirname(__FILE__)) . "/commits/frame" . $counter . ".html";
