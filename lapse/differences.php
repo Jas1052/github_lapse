bed087f5fdaee8e64a1cf872d6a8ba1db47f303c
Unknown <2017jliu@tjhsst.edu>
2017-02-16 15:54:39 -0500

    Started to use class.diff

--- lapse/differences.php
+++ lapse/differences.php
@@ -1,26 +1,7 @@
 <?php
+// include the Diff class
+require_once './class.Diff.php';
 
-$string1 = "The quick brown fox jumps over the lazy dog.";
-$string2 = "The quick brown albino fox jumps the groovy dog.";
-
-$string1 = explode(" ", $string1);
-$string2 = explode(" ", $string2);
-
-$diff = array_intersect($string2, $string1);
-
-$tmp = array();
-foreach ($string2 as $k => $w) {
-    if ($diff[$k]==$w) {
-        $tmp[$k] = $w;
-    }
-    else {
-        $tmp[$k] = "<b>$w</b>";
-    }
-}
-$diff = array_diff($string1, $tmp);
-
-foreach ($diff as $k => $w) {
-    $tmp[$k] .= " [<strike>$w</strike>]";
-}
-
-echo join (' ', $tmp);
+// output the result of comparing two files as HTML
+$file = fopen("differences.html","w");
+echo fwrite($file, Diff::toHTML(Diff::compareFiles('commits/frame2.html', 'commits/frame3.html')));
